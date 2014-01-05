6961cd85b955e72f9116b0604258d22ba7604b24
Saleem Shafi <saleemshafi@gmail.com>
2014-01-04 22:32:42 -0600

    getting the /api/words to respond

--- quran-words-site/web.js
+++ quran-words-site/web.js
@@ -1,9 +1,26 @@
 var restify = require('restify');
+var quran = require('../corpus-to-mongo/quranic-corpus-morphology-0.4.js');
+var _ = require('underscore');
+
+function getWords(req, res, next) {
+    var memorized_words = quran.tokens.filter( function(token) { return token.location.chapter >= 0 } );
+    var mem_words = memorized_words.map(function(token) { return {"word": token.lemma_tr ? token.lemma_tr : token.form_tr, "tag":token.tag}; });
+    var grouped_words = _.chain(mem_words).groupBy('word').pairs().value();
+    var sorted_words = _.chain(grouped_words)
+        .map( function(w) { return {"word":w[0], "count":w[1].length} } )
+        .sortBy('count')
+        .reverse()
+        .take(10)
+        .value();
+    res.charSet('utf-8');
+    res.send(sorted_words);
+    return next();
+}
 
 var server = restify.createServer();
 server.use(restify.queryParser({ mapParams: false }));
 
-server.get('/api/', function() {});
+server.get('/api/words', getWords);
 server.get(/\/?.*/, restify.serveStatic({
   directory: './public'
 }));
